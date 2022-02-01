f0016833574f9840876ce8db98219335be32ca26
Krivtsov Pavel <pkrivtsov6@gmail.com>
2022-02-01 11:33:46 +0300

    initial commit

---
+++ DiaryToDo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DiaryToDo
+//
+//  Created by Pavlentiy on 01.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
