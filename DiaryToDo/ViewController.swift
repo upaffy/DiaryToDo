0d38f2f79f48163e8d2940d6f8e050eb21d1aa14
Krivtsov Pavel <pkrivtsov6@gmail.com>
2022-02-05 19:09:57 +0300

    refused to implement the VIPER calendar

--- DiaryToDo/ViewController.swift
+++ DiaryToDo/ViewController.swift
@@ -8,10 +8,33 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private lazy var calendar: CalendarSelectorViewController = {
+        let layout = UICollectionViewFlowLayout()
+        layout.minimumLineSpacing = 0
+        layout.minimumInteritemSpacing = 0
+
+        return CalendarSelectorViewController(frame: .zero, collectionViewLayout: layout)
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .gray
+        
+        view.addSubview(calendar)
+        
+        NSLayoutConstraint.activate([
+            calendar.leadingAnchor.constraint(
+              equalTo: view.readableContentGuide.leadingAnchor),
+            calendar.trailingAnchor.constraint(
+              equalTo: view.readableContentGuide.trailingAnchor),
+            calendar.centerYAnchor.constraint(
+              equalTo: view.centerYAnchor,
+              constant: 10),
+            calendar.heightAnchor.constraint(
+              equalTo: view.heightAnchor,
+              multiplier: 0.5)
+        ])
     }
 
 }
