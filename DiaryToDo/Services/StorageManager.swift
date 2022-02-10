0ea58d8b41a099ed95a9ce7d70972982720ec259
Krivtsov Pavel <pkrivtsov6@gmail.com>
2022-02-10 17:26:29 +0300

    added manager to database interaction

--- DiaryToDo/Services/StorageManager.swift
+++ DiaryToDo/Services/StorageManager.swift
@@ -5,4 +5,20 @@
 //  Created by Pavlentiy on 10.02.2022.
 //
 
-import Foundation
+import RealmSwift
+
+class StorageManager {
+    static let shared = StorageManager()
+    
+    private var tasks: Results<TaskRealm>!
+    
+    // swiftlint:disable:next force_try
+    private let realm = try! Realm()
+    
+    private init() {}
+    
+    func fetchTasks(completion: @escaping (_ tasks: Results<TaskRealm>) -> Void) {
+        tasks = realm.objects(TaskRealm.self)
+        completion(tasks)
+    }
+}
